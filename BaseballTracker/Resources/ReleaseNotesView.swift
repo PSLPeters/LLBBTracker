eda50985ce67217accb1990c3a81fce0b37e9775
Michael Peters <greendakota99@yahoo.com>
2025-03-29 11:52:04 -0400

    1.1 (1) : - Slight adjustments to the release notes text.

--- BaseballTracker/Resources/ReleaseNotesView.swift
+++ BaseballTracker/Resources/ReleaseNotesView.swift
@@ -18,9 +18,9 @@ struct ReleaseNotesView: View {
             Text(
             """
             **Build 1.1 (1)**:
-            - New icon added - "BaseBilly"!
-            - Added Rate App button to easily rate the app in the App Store.
-            - Added Share App button to easily share the app with friends and family.
+            - New icon added - "BaseBilly", courtesy of DabbleArt!
+            - Added Rate App button to allow you to easily rate the app in the App Store.
+            - Added Share App button to help you quickly share the app with your fellow parents, coaches and friends.
             
             **Build 1.0 (1)**:
             - \(Image(systemName: "baseball")) Initial release of \(Constants.appName.replacingOccurrences(of: "©", with: ""))!
